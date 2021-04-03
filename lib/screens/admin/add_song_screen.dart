bcf47166f7431252593a2f4c29c935ddbea3645a
emeban44 <emi.cazorla@hotmail.com>
2021-04-03 14:42:24 +0200

    add song check boxes updating dynamically

--- lib/screens/admin/add_song_screen.dart
+++ lib/screens/admin/add_song_screen.dart
@@ -5,52 +5,145 @@ import '../../models/genres.dart';
 
 class AddSongScreen extends StatelessWidget {
   static const routeName = '/add-song';
+  static final List<bool> isChecked = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ];
+
+  void toggleChecked(int index) {
+    isChecked[index] = !isChecked[index];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          colors: [
-            Color.fromRGBO(10, 5, 27, 0.9),
-            Color.fromRGBO(33, 98, 131, 0.9),
-          ],
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color.fromRGBO(10, 5, 27, 0.9),
+              Color.fromRGBO(33, 98, 131, 0.9),
+            ],
+          ),
         ),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          title: Text('Add New Song'),
+        child: Scaffold(
           backgroundColor: Colors.transparent,
-        ),
-        body: Form(
-          child: Padding(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              children: [
-                SongTextInput('Song Name'),
-                SongTextInput('Song Artist'),
-                Text(
-                  'Song Name & Artist',
-                  style: TextStyle(color: Colors.grey.shade300),
-                ),
-                Divider(
-                  color: Colors.black,
-                  thickness: 1,
-                ),
-                Column(
+          appBar: AppBar(
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 18.0),
+                child: Icon(Icons.add),
+              ),
+            ],
+            title: Text('Add New Song'),
+            backgroundColor: Colors.transparent,
+          ),
+          body: SingleChildScrollView(
+            child: Form(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Column(
                   children: [
-                    Row(
-                      children: [
-                        GenreCheckBox(Genres.genres[0]),
-                        GenreCheckBox(Genres.genres[1]),
-                        GenreCheckBox(Genres.genres[2]),
-                      ],
-                    )
+                    SongTextInput('Song Name'),
+                    SongTextInput('Song Artist'),
+                    Text(
+                      'Song Name & Artist',
+                      style: TextStyle(color: Colors.grey.shade300),
+                    ),
+                    Divider(
+                      color: Colors.black,
+                      thickness: 1,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 20,
+                        bottom: 20,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              GenreCheckBox(Genres.genres[0], toggleChecked, 0),
+                              GenreCheckBox(Genres.genres[1], toggleChecked, 1),
+                              GenreCheckBox(Genres.genres[2], toggleChecked, 2),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              GenreCheckBox(Genres.genres[3], toggleChecked, 3),
+                              GenreCheckBox(Genres.genres[4], toggleChecked, 4),
+                              GenreCheckBox(Genres.genres[5], toggleChecked, 5),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              GenreCheckBox(Genres.genres[6], toggleChecked, 6),
+                              GenreCheckBox(Genres.genres[7], toggleChecked, 7),
+                              GenreCheckBox(Genres.genres[8], toggleChecked, 8),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Text(
+                      'Pick Song Genres',
+                      style: TextStyle(
+                        color: Colors.grey.shade300,
+                      ),
+                    ),
+                    Divider(
+                      color: Colors.black,
+                      thickness: 1,
+                    ),
+                    Container(
+                      width: 190,
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            print(isChecked);
+                          },
+                          icon: Icon(
+                            Icons.add,
+                            color: Colors.black,
+                          ),
+                          label: Text(
+                            'Add Song',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: 'Lexend',
+                              fontSize: 17,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.pink.shade200,
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
-                )
-              ],
+                ),
+              ),
             ),
           ),
         ),
--- lib/widgets/admin/genre_check_box.dart
+++ lib/widgets/admin/genre_check_box.dart
@@ -1,23 +1,51 @@
 import 'package:flutter/material.dart';
 
-class GenreCheckBox extends StatelessWidget {
+class GenreCheckBox extends StatefulWidget {
   final String genre;
+  final Function(int index) toggleCheckStatus;
+  final int genreIndex;
 
-  GenreCheckBox(this.genre);
+  GenreCheckBox(this.genre, this.toggleCheckStatus, this.genreIndex);
+
+  @override
+  _GenreCheckBoxState createState() => _GenreCheckBoxState();
+}
+
+class _GenreCheckBoxState extends State<GenreCheckBox> {
+  bool _isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        setState(() {
+          _isChecked = !_isChecked;
+        });
+        widget.toggleCheckStatus(widget.genreIndex);
+      },
       child: Container(
-        height: 100,
+        height: 80,
         width: 100,
         child: Column(
           children: [
-            Text(genre),
+            Text(
+              widget.genre,
+              style: TextStyle(
+                color: Colors.pink.shade200,
+                fontSize: 16,
+              ),
+            ),
             IconButton(
-              icon: Icon(Icons.check_box_outline_blank),
-              onPressed: () {},
+              icon: Icon(
+                _isChecked ? Icons.check_box : Icons.check_box_outline_blank,
+                color: _isChecked ? Colors.pink.shade200 : Colors.black,
+              ),
+              onPressed: () {
+                setState(() {
+                  _isChecked = !_isChecked;
+                });
+                widget.toggleCheckStatus(widget.genreIndex);
+              },
             )
           ],
         ),
