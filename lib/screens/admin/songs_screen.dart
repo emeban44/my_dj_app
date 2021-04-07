12fd6fdbfd214da2c04b304ffef504f736cc9274
emeban44 <emi.cazorla@hotmail.com>
2021-04-07 11:39:35 +0200

    search input box design

--- lib/screens/admin/songs_screen.dart
+++ lib/screens/admin/songs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../widgets/admin/songs_by_genre.dart';
 import '../../widgets/admin/genre_item.dart';
@@ -12,6 +13,8 @@ class SongsScreen extends StatefulWidget {
 class _SongsScreenState extends State<SongsScreen> {
   var showByGenre = false;
   var genre;
+  bool searching = false;
+  final _searchController = TextEditingController();
 
   void toggleGenres(String genreFrom) {
     setState(() {
@@ -26,25 +29,83 @@ class _SongsScreenState extends State<SongsScreen> {
     });
   }
 
+  void searchSongs() {
+    if (_searchController.text.isEmpty) return;
+  }
+
   @override
   Widget build(BuildContext context) {
     return showByGenre
         ? SongsByGenre(genre, goBack)
-        : GridView(
-            padding: const EdgeInsets.all(25),
-            children: Genres.genres
-                .map((genreData) => GenreItem(
-                      genreData,
-                      Colors.pink,
-                      toggleGenres,
-                    ))
-                .toList(),
-            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20,
-            ),
+        : Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 10),
+                margin:
+                    EdgeInsets.only(top: 15, right: 25, left: 25, bottom: 5),
+                //    color: Colors.purple,
+                height: 40,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.blue.shade100, Colors.pink.shade200],
+                    )),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      flex: 4,
+                      child: TextFormField(
+                        controller: _searchController,
+                        keyboardType: TextInputType.text,
+                        textCapitalization: TextCapitalization.words,
+                        style: TextStyle(
+                          fontFamily: 'Lexend',
+                          //    fontWeight: FontWeight.w600,
+                        ),
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Search',
+                          hintStyle: TextStyle(fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Container(
+                        padding: EdgeInsets.only(bottom: 5),
+                        child: IconButton(
+                            icon: Icon(Icons.search), onPressed: () {}),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: GridView(
+                  padding: const EdgeInsets.only(
+                      right: 25, left: 25, bottom: 25, top: 15),
+                  children: Genres.genres
+                      .map((genreData) => GenreItem(
+                            genreData,
+                            Colors.pink,
+                            toggleGenres,
+                          ))
+                      .toList(),
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 200,
+                    childAspectRatio: 3 / 2,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                  ),
+                ),
+              ),
+            ],
           );
   }
 }
